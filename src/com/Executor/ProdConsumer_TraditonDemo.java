55d7cc94cb3d4ecc97216895f6ec0bdadb351edb
shaoqingfeng <124175864@qq.com>
2021-02-03 17:23:29 +0800

    第三次修改

--- src/com/Executor/ProdConsumer_TraditonDemo.java
+++ src/com/Executor/ProdConsumer_TraditonDemo.java
@@ -14,6 +14,12 @@ import java.util.concurrent.locks.ReentrantLock;
  * 2 判断  干活
  * 通知  防止虚假唤醒机制
  * 多线程循环判断必须用 while 不能用if ，2个线程if可以，大于2个线程会出现异常，API文档中有详细 说明
+ * 原因：
+ * 在线程中notify或者notifyAll会唤醒一个或多个线程，当线程被唤醒后，被唤醒的线程继续执行阻塞后的操作。
+ *
+ * 这里分析一下get操纵： 当某个线程得到锁时number为0，此时它应该wait，下次被唤醒时（任意线程调用notify），
+ * number可能还是0的。因为有可能其他线程清空了number。如果此时用的是if它将不再判断number是否为空，
+ * 直接继续，这样就引起了错误。但如果用while则每次被唤醒时都会先检查number是否为空【0】再继续，这样才是正确的操作；生产也是同一个道理。
  * @Version: 1.0
  */
 class  ShareData{
